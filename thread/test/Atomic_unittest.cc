e3f3104a84ba7c001c58cadc2f879a1b58f5d37e
Shuo Chen <chenshuo@chenshuo.com>
2010-08-27 00:16:08 +0800

    add AtomicInt32 unittest

--- thread/test/Atomic_unittest.cc
+++ thread/test/Atomic_unittest.cc
@@ -3,6 +3,7 @@
 
 int main()
 {
+  {
   muduo::AtomicInt64 a0;
   assert(a0.get() == 0);
   assert(a0.getAndAdd(1) == 0);
@@ -16,5 +17,22 @@ int main()
   assert(a0.addAndGet(-3) == 2);
   assert(a0.getAndSet(100) == 2);
   assert(a0.get() == 100);
+  }
+
+  {
+  muduo::AtomicInt32 a1;
+  assert(a1.get() == 0);
+  assert(a1.getAndAdd(1) == 0);
+  assert(a1.get() == 1);
+  assert(a1.addAndGet(2) == 3);
+  assert(a1.get() == 3);
+  assert(a1.incrementAndGet() == 4);
+  assert(a1.get() == 4);
+  a1.increment();
+  assert(a1.get() == 5);
+  assert(a1.addAndGet(-3) == 2);
+  assert(a1.getAndSet(100) == 2);
+  assert(a1.get() == 100);
+  }
 }
 
