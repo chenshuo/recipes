c903e3e814c17ce5089999d727fcd50feeb0985d
Shuo Chen <chenshuo@chenshuo.com>
2011-04-30 10:24:06 +0800

    copy s01 from s00

---
+++ reactor/s01/EventLoop.cc
@@ -0,0 +1,52 @@
+#include "EventLoop.h"
+
+#include "logging/Logging.h"
+
+#include <assert.h>
+#include <poll.h>
+
+using namespace muduo;
+
+__thread EventLoop* t_loopInThisThread = 0;
+
+EventLoop::EventLoop()
+  : looping_(false),
+    threadId_(CurrentThread::tid())
+{
+  LOG_TRACE << "EventLoop created " << this << " in thread " << threadId_;
+  if (t_loopInThisThread)
+  {
+    LOG_FATAL << "Another EventLoop " << t_loopInThisThread
+              << " exists in this thread " << threadId_;
+  }
+  else
+  {
+    t_loopInThisThread = this;
+  }
+}
+
+EventLoop::~EventLoop()
+{
+  assert(!looping_);
+  t_loopInThisThread = NULL;
+}
+
+void EventLoop::loop()
+{
+  assert(!looping_);
+  assertInLoopThread();
+  looping_ = true;
+
+  ::poll(NULL, 0, 5*1000);
+
+  LOG_TRACE << "EventLoop " << this << " stop looping";
+  looping_ = false;
+}
+
+void EventLoop::abortNotInLoopThread()
+{
+  LOG_FATAL << "EventLoop::abortNotInLoopThread - EventLoop " << this
+            << " was created in threadId_ = " << threadId_
+            << ", current thread id = " <<  CurrentThread::tid();
+}
+
