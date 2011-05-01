cc7f415c322642767df57022aeed35f9ccc0ecdf
Shuo Chen <chenshuo@chenshuo.com>
2011-05-01 20:39:04 +0800

    copy s03 from s02

---
+++ reactor/s03/test2.cc
@@ -0,0 +1,17 @@
+#include "EventLoop.h"
+
+muduo::EventLoop* g_loop;
+
+void threadFunc()
+{
+  g_loop->loop();
+}
+
+int main()
+{
+  muduo::EventLoop loop;
+  g_loop = &loop;
+  muduo::Thread t(threadFunc);
+  t.start();
+  t.join();
+}
