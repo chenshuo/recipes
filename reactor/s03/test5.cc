9807252e909832279df3e753a17c43d92c2eb152
Shuo Chen <chenshuo@chenshuo.com>
2011-05-01 23:16:17 +0800

    rename test5 to test6, add test5.cc for runInLoop() in same thread.

--- reactor/s03/test5.cc
+++ reactor/s03/test5.cc
@@ -1,23 +1,44 @@
 #include "EventLoop.h"
-#include "EventLoopThread.h"
 #include <stdio.h>
 
-void runInThread()
+muduo::EventLoop* g_loop;
+int g_flag = 0;
+
+void run4()
 {
-  printf("runInThread(): pid = %d, tid = %d\n", getpid(), muduo::CurrentThread::tid());
+  printf("run4(): pid = %d, flag = %d\n", getpid(), g_flag);
+  g_loop->quit();
+}
+
+void run3()
+{
+  printf("run3(): pid = %d, flag = %d\n", getpid(), g_flag);
+  g_loop->runAfter(3, run4);
+  g_flag = 3;
+}
+
+void run2()
+{
+  printf("run2(): pid = %d, flag = %d\n", getpid(), g_flag);
+  g_loop->queueInLoop(run3);
+}
+
+void run1()
+{
+  g_flag = 1;
+  printf("run1(): pid = %d, flag = %d\n", getpid(), g_flag);
+  g_loop->runInLoop(run2);
+  g_flag = 2;
 }
 
 int main()
 {
-  printf("main(): pid = %d, tid = %d\n", getpid(), muduo::CurrentThread::tid());
+  printf("main(): pid = %d, flag = %d\n", getpid(), g_flag);
 
-  muduo::EventLoopThread loopThread;
-  muduo::EventLoop* loop = loopThread.startLoop();
-  loop->runInLoop(runInThread);
-  sleep(1);
-  loop->runAfter(2, runInThread);
-  sleep(3);
-  loop->quit();
+  muduo::EventLoop loop;
+  g_loop = &loop;
 
-  printf("exit main().\n");
+  loop.runAfter(2, run1);
+  loop.loop();
+  printf("main(): pid = %d, flag = %d\n", getpid(), g_flag);
 }
