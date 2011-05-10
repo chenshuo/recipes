f56e876aa417f4669d40803f3126838a424e952c
Shuo Chen <chenshuo@chenshuo.com>
2011-05-10 22:32:08 +0800

    Keep test3.cc all the same across steps.

--- reactor/s02/test3.cc
+++ reactor/s02/test3.cc
@@ -17,6 +17,17 @@ int main()
   muduo::EventLoop loop;
   g_loop = &loop;
 
-  loop.runAfter(5, timeout);
+  int timerfd = ::timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK | TFD_CLOEXEC);
+  muduo::Channel channel(&loop, timerfd);
+  channel.setReadCallback(timeout);
+  channel.enableReading();
+
+  struct itimerspec howlong;
+  bzero(&howlong, sizeof howlong);
+  howlong.it_value.tv_sec = 5;
+  ::timerfd_settime(timerfd, 0, &howlong, NULL);
+
   loop.loop();
+
+  ::close(timerfd);
 }
