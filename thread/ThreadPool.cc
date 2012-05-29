chenshuo/recipes

commit 1ae58bf4e72708f93b5f6a331bee1fead48243ff

add sort03.cc, multithreaded, 49% faster than sort(1).

thread/ThreadPool.cc
@@ -24,6 +24,7 @@ ThreadPool::ThreadPool(const std::string& name)
 
 ThreadPool::~ThreadPool()
 {
+  stop();
 }
 
 void ThreadPool::start(int numThreads)

esort/build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-g++ -I .. -O2 -g -o sort00 sort00.cc ../datetime/Timestamp.cc
-g++ -I .. -O2 -g -o sort01 sort01.cc ../datetime/Timestamp.cc
-g++ -I .. -O2 -g -o sort02 sort02.cc ../datetime/Timestamp.cc
+set -x
+
+CXXFLAGS="-O2 -g -Wall"
+export CXXFLAGS
+g++ -I .. $CXXFLAGS -o sort00 sort00.cc ../datetime/Timestamp.cc
+g++ -I .. $CXXFLAGS -o sort01 sort01.cc ../datetime/Timestamp.cc
+g++ -I .. $CXXFLAGS -o sort02 sort02.cc ../datetime/Timestamp.cc
+g++ -I .. $CXXFLAGS -o sort03 sort03.cc ../datetime/Timestamp.cc ../thread/Thread.cc ../thread/ThreadPool.cc ../thread/Exception.cc -lpthread
+g++ -I .. $CXXFLAGS -o sort04 sort04.cc ../datetime/Timestamp.cc ../thread/Thread.cc ../thread/ThreadPool.cc ../thread/Exception.cc -lpthread
 

esort/sort01.cc
@@ -1,4 +1,5 @@
 // version 01: only be able to sort 1GB file
+// sort with keys
 // 30% faster than sort(1) for 1GB file.
 
 #include <boost/noncopyable.hpp>

esort/sort03.cc
@@ -1,30 +1,29 @@
-// version 02: first impl to sort large files.
-// sort and merge
-// 30% faster than sort(1) for all 1GB 5GB 10GB files
+// version 03: pipeline impl to sort large files.
 
 #include <boost/noncopyable.hpp>
 #include <boost/ptr_container/ptr_vector.hpp>
+#include <boost/bind.hpp>
+#include <boost/enable_shared_from_this.hpp>
 
 #include <datetime/Timestamp.h>
+#include <thread/ThreadPool.h>
+#include <thread/BlockingQueue.h>
 
 #include <algorithm>
 #include <string>
-#include <ext/vstring.h>
+//#include <ext/vstring.h>
 #include <vector>
 
 #include <assert.h>
 #include <stdio.h>
-#include <string.h>
 
 #include <sys/resource.h>
 
 
-// typedef std::string string;
-typedef __gnu_cxx::__sso_string string;
+typedef std::string string;
+// typedef __gnu_cxx::__sso_string string;
 
 using muduo::Timestamp;
-
-#define TMP_DIR "/tmp/"
 
 class InputFile : boost::noncopyable
 {
@@ -151,9 +150,10 @@ struct Key
   }
 };
 
-void sort(const std::vector<string>& data, std::vector<Key>* keys)
+void sortWithKeys(const std::vector<string>& data, std::vector<Key>* keys)
 {
   Timestamp start = Timestamp::now();
+  keys->clear();
   keys->reserve(data.size());
 
   for (size_t i = 0; i < data.size(); ++i)
@@ -165,45 +165,140 @@ void sort(const std::vector<string>& data, std::vector<Key>* keys)
   std::sort(keys->begin(), keys->end());
 }
 
-int sortSplit(const char* filename)
+typedef std::vector<string> Data;
+
+class Task;
+typedef boost::shared_ptr<Task> TaskPtr;
+
+class Task : public boost::enable_shared_from_this<Task>
 {
-  std::vector<string> data;
-  // read
-  InputFile in(filename);
-  int batch = 0;
-  while(true)
+ public:
+  Task(muduo::BlockingQueue<TaskPtr>* queue)
+    : queue_(queue),
+      id_(s_created++),
+      sorted_(false)
   {
-    Timestamp startThis = Timestamp::now();
-    readInput(in, &data);
+    assert(muduo::CurrentThread::isMainThread());
+    printf("Task %d\n", id_);
+  }
 
+  ~Task()
+  {
+    printf("~Task %d\n", id_);
+  }
+
+  bool read(InputFile& in)
+  {
+    assert(muduo::CurrentThread::isMainThread());
+    sorted_ = false;
+    Timestamp startRead = Timestamp::now();
+    printf("task %d start read  %s\n", id_, startRead.toString().c_str());
+    readInput(in, &data_);
     Timestamp readDone = Timestamp::now();
-    printf("%zd\nread  %f\n", data.size(), timeDifference(readDone, startThis));
-    if (data.empty())
-    {
-      break;
-    }
+    printf("task %d read done   %s  %f  %zd \n",
+        id_, readDone.toString().c_str(), timeDifference(readDone, startRead), data_.size());
+    return !data_.empty();
+  }
 
-    std::vector<Key> keys;
-    sort(data, &keys);
-
+  void sort()
+  {
+    // assert(!muduo::CurrentThread::isMainThread());
+    assert(!sorted_);
+    Timestamp startSort = Timestamp::now();
+    printf("task %d start sort  %s\n", id_, startSort.toString().c_str());
+    sortWithKeys(data_, &keys_);
+    sorted_ = true;
     Timestamp sortDone = Timestamp::now();
-    printf("sort  %f\n", timeDifference(sortDone, readDone));
+    printf("task %d sort done   %s  %f\n",
+        id_, sortDone.toString().c_str(), timeDifference(sortDone, startSort));
+    queue_->put(shared_from_this());
+  }
 
-    // output
+  void write(int batch)
+  {
+    assert(muduo::CurrentThread::isMainThread());
+    assert(sorted_);
+
+    Timestamp startWrite = Timestamp::now();
+    printf("task %d start write %s\n", id_, startWrite.toString().c_str());
     {
       char output[256];
-      snprintf(output, sizeof output, TMP_DIR "tmp%d", batch++);
+      snprintf(output, sizeof output, "tmp%d", batch);
 
       OutputFile out(output);
-      for (std::vector<Key>::iterator it = keys.begin();
-          it != keys.end();
+      for (std::vector<Key>::iterator it = keys_.begin();
+          it != keys_.end();
           ++it)
       {
-        out.writeLine(data[it->index]);
+        out.writeLine(data_[it->index]);
       }
     }
     Timestamp writeDone = Timestamp::now();
-    printf("write %f\n", timeDifference(writeDone, sortDone));
+    printf("task %d write done  %s  %f\n",
+        id_, writeDone.toString().c_str(), timeDifference(writeDone, startWrite));
+  }
+
+  const Data& data() const
+  {
+    return data_;
+  }
+
+ private:
+  Data data_;
+  std::vector<Key> keys_;
+  muduo::BlockingQueue<TaskPtr>* queue_;
+  int id_;
+  bool sorted_;
+
+  static int s_created;
+};
+
+int Task::s_created = 0;
+
+
+int sortSplit(const char* filename)
+{
+  // read
+  InputFile in(filename);
+  muduo::BlockingQueue<TaskPtr> queue;
+  muduo::ThreadPool threadPool;
+  threadPool.start(2);
+  int active = 0;
+
+  // initialize
+  {
+    TaskPtr task(new Task(&queue));
+    if (task->read(in))
+    {
+      threadPool.run(boost::bind(&Task::sort, task));
+      active++;
+    }
+    else
+    {
+      return 0;
+    }
+
+    TaskPtr task2(new Task(&queue));
+    if (task2->read(in))
+    {
+      threadPool.run(boost::bind(&Task::sort, task2));
+      active++;
+    }
+  }
+
+  int batch = 0;
+  while (active > 0)
+  {
+    TaskPtr task = queue.take();
+    active--;
+
+    task->write(batch++);
+
+    if (task->read(in))
+    {
+      threadPool.run(boost::bind(&Task::sort, task));
+      active++;
+    }
   }
   return batch;
 }
@@ -239,7 +334,7 @@ void merge(const int batch)
   for (int i = 0; i < batch; ++i)
   {
     char filename[128];
-    snprintf(filename, sizeof filename, TMP_DIR "tmp%d", i);
+    snprintf(filename, sizeof filename, "tmp%d", i);
     inputs.push_back(new InputFile(filename));
     Record rec(&inputs.back());
     if (rec.next())
@@ -271,23 +366,26 @@ int main(int argc, char* argv[])
   bool kKeepIntermediateFiles = false;
 
   {
-    // set max virtual memory to 3GB.
+    // set max virtual memory to 4GB.
     size_t kOneGB = 1024*1024*1024;
-    rlimit rl = { 3.0*kOneGB, 3.0*kOneGB };
+    rlimit rl = { 4.0*kOneGB, 4.0*kOneGB };
     setrlimit(RLIMIT_AS, &rl);
   }
 
   Timestamp start = Timestamp::now();
+  printf("sortSplit start %s\n", start.toString().c_str());
 
   // sort
   int batch = sortSplit(argv[1]);
   Timestamp sortDone = Timestamp::now();
-  printf("sortSplit %f\n", timeDifference(sortDone, start));
+  printf("sortSplit done %f\n", timeDifference(sortDone, start));
 
-  if (batch == 1)
+  if (batch == 0)
   {
-    unlink("output");
-    rename(TMP_DIR "tmp0", "output");
+  }
+  else if (batch == 1)
+  {
+    rename("tmp0", "output");
   }
   else
   {
@@ -302,7 +400,7 @@ int main(int argc, char* argv[])
     for (int i = 0; i < batch; ++i)
     {
       char tmp[256];
-      snprintf(tmp, sizeof tmp, TMP_DIR "tmp%d", i);
+      snprintf(tmp, sizeof tmp, "tmp%d", i);
       unlink(tmp);
     }
   }

esort/sort04.cc
@@ -1,0 +1,388 @@
+// version 03: pipeline impl to sort large files.
+// reduce memory usage
+// larger buffer
+
+#include <boost/noncopyable.hpp>
+#include <boost/ptr_container/ptr_vector.hpp>
+#include <boost/bind.hpp>
+#include <boost/enable_shared_from_this.hpp>
+
+#include <datetime/Timestamp.h>
+#include <thread/ThreadPool.h>
+#include <thread/BlockingQueue.h>
+
+#include <algorithm>
+#include <vector>
+
+#include <assert.h>
+#include <stdio.h>
+
+#include <sys/resource.h>
+
+
+using muduo::Timestamp;
+
+class InputFile : boost::noncopyable
+{
+ public:
+  InputFile(const char* filename)
+    : file_(fopen(filename, "rb"))
+  {
+    assert(file_);
+    setbuffer(file_, buffer_, sizeof buffer_);
+    // posix_fadvise for initial input
+    // http://lwn.net/Articles/449420/
+    // http://blog.mikemccandless.com/2010/06/lucene-and-fadvisemadvise.html
+  }
+
+  ~InputFile()
+  {
+    fclose(file_);
+  }
+
+  int read(char* buf, int size)
+  {
+    return fread_unlocked(buf, 1, size, file_);
+  }
+
+ private:
+  FILE* file_;
+  char buffer_[4*1024*1024];
+};
+
+const int kRecordSize = 100;
+class OutputFile : boost::noncopyable
+{
+ public:
+  OutputFile(const char* filename)
+    : file_(fopen(filename, "wb"))
+  {
+    assert(file_);
+    setbuffer(file_, buffer_, sizeof buffer_);
+    // posix_fadvise for final output
+  }
+
+  ~OutputFile()
+  {
+    // how to use OS buffers sensibly ?
+    // fdatasync(fileno_unlocked(file_));
+    fclose(file_);
+  }
+
+  void writeRecord(char (&record)[kRecordSize])
+  {
+    fwrite_unlocked(record, 1, kRecordSize, file_);
+  }
+
+ private:
+  FILE* file_;
+  char buffer_[4*1024*1024];
+};
+
+const bool kUseReadLine = false;
+const int kBatchRecords = 10000000;
+
+struct Record
+{
+  char data[kRecordSize];
+};
+
+typedef std::vector<Record> Data;
+
+void readInput(InputFile& in, Data* data)
+{
+  int64_t totalSize = 0;
+  data->clear();
+  data->reserve(kBatchRecords);
+
+  for (int i = 0; i < kBatchRecords; ++i)
+  {
+    Record record;
+    if (int n = in.read(record.data, sizeof record.data))
+    {
+      assert (n == kRecordSize);
+      totalSize += n;
+      data->push_back(record);
+    }
+    else
+    {
+      break;
+    }
+  }
+}
+
+struct Key
+{
+  char key[10];
+  int index;
+
+  Key(const Record& record, int idx)
+    : index(idx)
+  {
+    memcpy(key, record.data, 10);
+  }
+
+  bool operator<(const Key& rhs) const
+  {
+    return memcmp(key, rhs.key, sizeof key) < 0;
+  }
+};
+
+void sortWithKeys(const Data& data, std::vector<Key>* keys)
+{
+  Timestamp start = Timestamp::now();
+  keys->clear();
+  keys->reserve(data.size());
+
+  for (size_t i = 0; i < data.size(); ++i)
+  {
+    keys->push_back(Key(data[i], i));
+  }
+  // printf("make keys %f\n", data.size(), timeDifference(Timestamp::now(), start));
+
+  std::sort(keys->begin(), keys->end());
+}
+
+
+class Task;
+typedef boost::shared_ptr<Task> TaskPtr;
+
+class Task : public boost::enable_shared_from_this<Task>
+{
+ public:
+  Task(muduo::BlockingQueue<TaskPtr>* queue)
+    : queue_(queue),
+      id_(s_created++),
+      sorted_(false)
+  {
+    assert(muduo::CurrentThread::isMainThread());
+    printf("Task %d\n", id_);
+  }
+
+  ~Task()
+  {
+    printf("~Task %d\n", id_);
+  }
+
+  bool read(InputFile& in)
+  {
+    assert(muduo::CurrentThread::isMainThread());
+    sorted_ = false;
+    Timestamp startRead = Timestamp::now();
+    printf("task %d start read  %s\n", id_, startRead.toString().c_str());
+    readInput(in, &data_);
+    Timestamp readDone = Timestamp::now();
+    printf("task %d read done   %s  %f  %zd \n",
+        id_, readDone.toString().c_str(), timeDifference(readDone, startRead), data_.size());
+    return !data_.empty();
+  }
+
+  void sort()
+  {
+    // assert(!muduo::CurrentThread::isMainThread());
+    assert(!sorted_);
+    Timestamp startSort = Timestamp::now();
+    printf("task %d start sort  %s\n", id_, startSort.toString().c_str());
+    sortWithKeys(data_, &keys_);
+    sorted_ = true;
+    Timestamp sortDone = Timestamp::now();
+    printf("task %d sort done   %s  %f\n",
+        id_, sortDone.toString().c_str(), timeDifference(sortDone, startSort));
+    queue_->put(shared_from_this());
+  }
+
+  void write(int batch)
+  {
+    assert(muduo::CurrentThread::isMainThread());
+    assert(sorted_);
+
+    Timestamp startWrite = Timestamp::now();
+    printf("task %d start write %s\n", id_, startWrite.toString().c_str());
+    {
+      char output[256];
+      snprintf(output, sizeof output, "tmp%d", batch);
+
+      OutputFile out(output);
+      for (std::vector<Key>::iterator it = keys_.begin();
+          it != keys_.end();
+          ++it)
+      {
+        out.writeRecord(data_[it->index].data);
+      }
+    }
+    Timestamp writeDone = Timestamp::now();
+    printf("task %d write done  %s  %f\n",
+        id_, writeDone.toString().c_str(), timeDifference(writeDone, startWrite));
+  }
+
+  const Data& data() const
+  {
+    return data_;
+  }
+
+ private:
+  Data data_;
+  std::vector<Key> keys_;
+  muduo::BlockingQueue<TaskPtr>* queue_;
+  int id_;
+  bool sorted_;
+
+  static int s_created;
+};
+
+int Task::s_created = 0;
+
+
+int sortSplit(const char* filename)
+{
+  // read
+  InputFile in(filename);
+  muduo::BlockingQueue<TaskPtr> queue;
+  muduo::ThreadPool threadPool;
+  threadPool.start(2);
+  int active = 0;
+
+  // initialize
+  {
+    TaskPtr task(new Task(&queue));
+    if (task->read(in))
+    {
+      threadPool.run(boost::bind(&Task::sort, task));
+      active++;
+    }
+    else
+    {
+      return 0;
+    }
+
+    TaskPtr task2(new Task(&queue));
+    if (task2->read(in))
+    {
+      threadPool.run(boost::bind(&Task::sort, task2));
+      active++;
+    }
+  }
+
+  int batch = 0;
+  while (active > 0)
+  {
+    TaskPtr task = queue.take();
+    active--;
+
+    task->write(batch++);
+
+    if (task->read(in))
+    {
+      threadPool.run(boost::bind(&Task::sort, task));
+      active++;
+    }
+  }
+  return batch;
+}
+
+struct Source
+{
+  char data[kRecordSize];
+  InputFile* input;
+
+  Source(InputFile* in)
+    : input(in)
+  {
+  }
+
+  bool next()
+  {
+    return input->read(data, sizeof data) == kRecordSize;
+  }
+
+  bool operator<(const Source& rhs) const
+  {
+    return memcmp(data, rhs.data, sizeof data) > 0;
+  }
+};
+
+void merge(const int batch)
+{
+  printf("merge %d files\n", batch);
+
+  boost::ptr_vector<InputFile> inputs;
+  std::vector<Source> keys;
+
+  for (int i = 0; i < batch; ++i)
+  {
+    char filename[128];
+    snprintf(filename, sizeof filename, "tmp%d", i);
+    inputs.push_back(new InputFile(filename));
+    Source rec(&inputs.back());
+    if (rec.next())
+    {
+      keys.push_back(rec);
+    }
+  }
+
+  OutputFile out("output");
+  std::make_heap(keys.begin(), keys.end());
+  while (!keys.empty())
+  {
+    std::pop_heap(keys.begin(), keys.end());
+    out.writeRecord(keys.back().data);
+
+    if (keys.back().next())
+    {
+      std::push_heap(keys.begin(), keys.end());
+    }
+    else
+    {
+      keys.pop_back();
+    }
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  bool kKeepIntermediateFiles = false;
+
+  printf("pid %d\n", getpid());
+
+  {
+    // set max virtual memory to 3GB.
+    size_t kOneGB = 1024*1024*1024;
+    rlimit rl = { 3.0*kOneGB, 3.0*kOneGB };
+    setrlimit(RLIMIT_AS, &rl);
+  }
+
+  Timestamp start = Timestamp::now();
+  printf("sortSplit start %s\n", start.toString().c_str());
+
+  // sort
+  int batch = sortSplit(argv[1]);
+  Timestamp sortDone = Timestamp::now();
+  printf("sortSplit done %f\n", timeDifference(sortDone, start));
+
+  if (batch == 0)
+  {
+  }
+  else if (batch == 1)
+  {
+    rename("tmp0", "output");
+  }
+  else
+  {
+    // merge
+    merge(batch);
+    Timestamp mergeDone = Timestamp::now();
+    printf("mergeSplit %f\n", timeDifference(mergeDone, sortDone));
+  }
+
+  if (!kKeepIntermediateFiles)
+  {
+    for (int i = 0; i < batch; ++i)
+    {
+      char tmp[256];
+      snprintf(tmp, sizeof tmp, "tmp%d", i);
+      unlink(tmp);
+    }
+  }
+
+  printf("total %f\n", timeDifference(Timestamp::now(), start));
+}

commit fceafe271c232f9ae430e9764ce65f57cffd7929

fix bug introduced in 1ae58bf4, only stop() in ThreadPool dtor when it hasn't stoped.

thread/ThreadPool.cc
@@ -24,7 +24,10 @@ ThreadPool::ThreadPool(const std::string& name)
 
 ThreadPool::~ThreadPool()
 {
-  stop();
+  if (running_)
+  {
+    stop();
+  }
 }
 
 void ThreadPool::start(int numThreads)