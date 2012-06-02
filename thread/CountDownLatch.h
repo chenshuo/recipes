b24dfd25fe19ca82e52326c9bc0aacccc008fff4
Shuo Chen <chenshuo@chenshuo.com>
2012-06-02 22:48:17 +0800

    inline CountDownLatch.cc

--- thread/CountDownLatch.h
+++ thread/CountDownLatch.h
@@ -20,13 +20,37 @@ class CountDownLatch : boost::noncopyable
 {
  public:
 
-  explicit CountDownLatch(int count);
+  explicit CountDownLatch(int count)
+    : mutex_(),
+      condition_(mutex_),
+      count_(count)
+  {
+  }
 
-  void wait();
+  void wait()
+  {
+    MutexLockGuard lock(mutex_);
+    while (count_ > 0)
+    {
+      condition_.wait();
+    }
+  }
 
-  void countDown();
+  void countDown()
+  {
+    MutexLockGuard lock(mutex_);
+    --count_;
+    if (count_ == 0)
+    {
+      condition_.notifyAll();
+    }
+  }
 
-  int getCount() const;
+  int getCount() const
+  {
+    MutexLockGuard lock(mutex_);
+    return count_;
+  }
 
  private:
   mutable MutexLock mutex_;
