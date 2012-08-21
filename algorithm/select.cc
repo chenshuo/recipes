4df1088fe949ffaa7a500231e4001cad9c63cad5
Shuo Chen <chenshuo@chenshuo.com>
2012-08-21 10:25:58 +0800

    add quick select with partial sort

--- algorithm/select.cc
+++ algorithm/select.cc
@@ -32,6 +32,18 @@ vector<int> selectByNthElement(const vector<int>& input, int k1, int k2)
   return vector<int>(temp.begin() + k1, temp.begin() + k2 + 1);
 }
 
+vector<int> selectByPartialSort(const vector<int>& input, int k1, int k2)
+{
+  assert(0 <= k1 && k1 < input.size());
+  assert(0 <= k2 && k2 < input.size());
+  assert(k1 <= k2);
+
+  vector<int> temp(input);
+  nth_element(temp.begin(), temp.begin() + k1, temp.end());
+  partial_sort(temp.begin() + k1, temp.begin() + k2 + 1, temp.end());
+  return vector<int>(temp.begin() + k1, temp.begin() + k2 + 1);
+}
+
 void print(const vector<int>& vec)
 {
   copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, ", "));
@@ -47,6 +59,7 @@ int main()
   }
   random_shuffle(input.begin(), input.end());
   print(input);
-  print(selectBySorting(input, 0, 99));
-  print(selectByNthElement(input, 0, 99));
+  print(selectBySorting(input, 10, 20));
+  print(selectByNthElement(input, 10, 20));
+  print(selectByPartialSort(input, 10, 20));
 }
