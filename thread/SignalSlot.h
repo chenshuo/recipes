2d6ff445e1b5e78b7e9d0c89e240037cf9d23ed9
Shuo Chen <chenshuo@chenshuo.com>
2012-10-14 11:55:26 +0800

    add signal slot

---
+++ thread/SignalSlot.h
@@ -0,0 +1,185 @@
+#ifndef MUDUO_BASE_SIGNALSLOT_H
+#define MUDUO_BASE_SIGNALSLOT_H
+
+#include "Mutex.h"
+
+#include <boost/function.hpp>
+#include <boost/noncopyable.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/weak_ptr.hpp>
+
+#include <vector>
+
+namespace muduo
+{
+
+namespace detail
+{
+
+template<typename Callback>
+struct SlotImpl;
+
+template<typename Callback>
+struct SignalImpl : boost::noncopyable
+{
+  typedef std::vector<boost::weak_ptr<SlotImpl<Callback> > > SlotList;
+
+  SignalImpl()
+    : slots_(new SlotList)
+  {
+  }
+
+  void copyOnWrite()
+  {
+    mutex_.assertLocked();
+    if (!slots_.unique())
+    {
+      slots_.reset(new SlotList(*slots_));
+    }
+    assert(slots_.unique());
+  }
+
+  void clean()
+  {
+    MutexLockGuard lock(mutex_);
+    copyOnWrite();
+    SlotList& list(*slots_);
+    typename SlotList::iterator it(list.begin());
+    while (it != list.end())
+    {
+      if (it->expired())
+      {
+        it = list.erase(it);
+      }
+      else
+      {
+        ++it;
+      }
+    }
+  }
+
+  MutexLock mutex_;
+  boost::shared_ptr<SlotList> slots_;
+};
+
+template<typename Callback>
+struct SlotImpl : boost::noncopyable
+{
+  typedef SignalImpl<Callback> Data;
+  SlotImpl(const boost::shared_ptr<Data>& data, const Callback& cb)
+    : data_(data), cb_(cb), tie_(), tied_(false)
+  {
+  }
+
+  SlotImpl(const boost::shared_ptr<Data>& data, const Callback& cb,
+      const boost::shared_ptr<void>& dest)
+    : data_(data), cb_(cb), tie_(dest), tied_(true)
+  {
+  }
+
+  ~SlotImpl()
+  {
+    boost::shared_ptr<Data> data(data_.lock());
+    if (data)
+    {
+      data->clean();
+    }
+  }
+
+  boost::weak_ptr<Data> data_;
+  Callback cb_;
+  boost::weak_ptr<void> tie_;
+  bool tied_;
+};
+
+}
+
+/// This is the handle for a slot
+///
+/// The slot will remain connected to the signal fot the life time of the
+/// returned Slot object (and its copies).
+typedef boost::shared_ptr<void> Slot;
+
+template<typename Signature>
+class Signal;
+
+template <typename RET, typename... ARGS>
+class Signal<RET(ARGS...)> : boost::noncopyable
+{
+ public:
+  typedef std::function<void (ARGS...)> Callback;
+  typedef detail::SignalImpl<Callback> SignalImpl;
+  typedef detail::SlotImpl<Callback> SlotImpl;
+
+  Signal()
+    : impl_(new SignalImpl)
+  {
+  }
+
+  ~Signal()
+  {
+  }
+
+  Slot connect(const Callback& func)
+  {
+    boost::shared_ptr<SlotImpl> slotImpl(new SlotImpl(impl_, func));
+    add(slotImpl);
+    return slotImpl;
+  }
+
+  Slot connect(const Callback& func, const boost::shared_ptr<void>& dest)
+  {
+    boost::shared_ptr<SlotImpl> slotImpl(new SlotImpl(impl_, func, dest));
+    add(slotImpl);
+    return slotImpl;
+  }
+
+  void call(ARGS&&... args)
+  {
+    SignalImpl& impl(*impl_);
+    boost::shared_ptr<typename SignalImpl::SlotList> slots;
+    {
+      MutexLockGuard lock(impl.mutex_);
+      slots = impl.slots_;
+    }
+    typename SignalImpl::SlotList& s(*slots);
+    for (typename SignalImpl::SlotList::const_iterator it = s.begin(); it != s.end(); ++it)
+    {
+      boost::shared_ptr<SlotImpl> slotImpl = it->lock();
+      if (slotImpl)
+      {
+        boost::shared_ptr<void> guard;
+        if (slotImpl->tied_)
+        {
+          guard = slotImpl->tie_.lock();
+          if (guard)
+          {
+            slotImpl->cb_(args...);
+          }
+        }
+        else
+        {
+          slotImpl->cb_(args...);
+        }
+      }
+    }
+  }
+
+ private:
+
+  void add(const boost::shared_ptr<SlotImpl>& slot)
+  {
+    SignalImpl& impl(*impl_);
+    {
+      MutexLockGuard lock(impl.mutex_);
+      impl.copyOnWrite();
+      impl.slots_->push_back(slot);
+    }
+  }
+
+  const boost::shared_ptr<SignalImpl> impl_;
+};
+
+}
+
+#endif // MUDUO_BASE_SIGNALSLOT_H
--- thread/test/SignalSlot_test.cc
+++ thread/test/SignalSlot_test.cc
@@ -1,3 +1,4 @@
+#include "../SignalSlot.h"
 #include "../SignalSlotTrivial.h"
 
 #include <boost/bind.hpp>
@@ -161,5 +162,97 @@ BOOST_AUTO_TEST_CASE(testSignalSlotTrivialString)
 
   printf("========\n");
   signal.call("hello");
+}
+
+BOOST_AUTO_TEST_CASE(testSignalSlotZero)
+{
+  muduo::Signal<void()> signal;
+
+  printf("==== testSignalSlotZero ====\n");
+  signal.call();
+
+  muduo::Slot s1 = signal.connect(&Foo::szero);
+
+  printf("========\n");
+  signal.call();
+
+  Foo f;
+  muduo::Slot s2 = signal.connect(boost::bind(&Foo::zero, &f));
+
+  printf("========\n");
+  signal.call();
+
+  muduo::Slot s3 = signal.connect(boost::bind(&Foo::one, &f, 42));
+
+  printf("========\n");
+  signal.call();
+
+  const Foo cf;
+  muduo::Slot s4 = signal.connect(boost::bind(&Foo::zeroc, &cf));
+
+  printf("========\n");
+  signal.call();
+
+  muduo::Slot s5 = signal.connect(boost::bind(&Foo::onec, &cf, 128));
+
+  printf("========\n");
+  signal.call();
+
+  s1 = muduo::Slot();
+  printf("========\n");
+  signal.call();
+
+
+  s4 = s3 = s2 = muduo::Slot();
+  printf("========\n");
+  signal.call();
+
+}
+
+BOOST_AUTO_TEST_CASE(testSignalSlotOne)
+{
+  muduo::Signal<void(int)> signal;
+
+  printf("========\n");
+  signal.call(50);
+
+  muduo::Slot s4;
+  {
+  muduo::Slot s1 = signal.connect(&Foo::sone);
+
+  printf("========\n");
+  signal.call(51);
+
+  Foo f;
+  muduo::Slot s2 = signal.connect(boost::bind(&Foo::one, &f, _1));
+
+  printf("========\n");
+  signal.call(52);
+
+  const Foo cf;
+  muduo::Slot s3 = signal.connect(boost::bind(&Foo::onec, &cf, _1));
+
+  printf("========\n");
+  signal.call(53);
+
+  s4 = s3;
+  }
+
+  printf("========\n");
+  signal.call(54);
+}
+
+BOOST_AUTO_TEST_CASE(testSignalSlotLife)
+{
+  muduo::Slot s1;
+
+  {
+  muduo::Signal<void()> signal;
+  s1 = signal.connect(&Foo::szero);
+
+  printf("========\n");
+  signal.call();
+  }
+
 }
 
