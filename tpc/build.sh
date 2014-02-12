ddbf26c37263f7231c8cddd09fe510999c7bf249
Shuo Chen <chenshuo@chenshuo.com>
2014-02-12 12:00:25 -0800

    current TPC echo

--- tpc/build.sh
+++ tpc/build.sh
@@ -4,6 +4,6 @@ SRCS="Acceptor.cc InetAddress.cc TcpStream.cc Socket.cc"
 
 set -x
 
-g++-4.7 -std=c++0x -Wall -Wextra -g -O2 $SRCS echo.cc -o echo
+g++-4.7 -std=c++0x -Wall -Wextra -g -O2 $SRCS echo.cc -o echo -lpthread
 
 g++-4.7 -std=c++0x -Wall -Wextra -g -O2 $SRCS ttcp.cc -o ttcp -lboost_program_options
--- tpc/echo.cc
+++ tpc/echo.cc
@@ -2,23 +2,35 @@
 #include "InetAddress.h"
 #include "TcpStream.h"
 
-// an iterative echo server
+#include <thread>
+
+// a thread-per-connection current echo server
 int main()
 {
   InetAddress listenAddr(3007);
   Acceptor acceptor(listenAddr);
+  printf("Accepting... Ctrl-C to exit\n");
+  int count = 0;
   while (true)
   {
     TcpStreamPtr tcpStream = acceptor.accept();
-    char buf[4096];
-    int nr = 0;
-    while ( (nr = tcpStream->readSome(buf, sizeof(buf))) > 0)
-    {
-      int nw = tcpStream->sendAll(buf, nr);
-      if (nw < nr)
+    printf("accepted no. %d client\n", ++count);
+
+    // C++11 doesn't allow capturing unique_ptr in lambda, C++14 allows.
+    std::thread thr([count] (TcpStreamPtr stream) {
+      printf("thread for no. %d client started.\n", count);
+      char buf[4096];
+      int nr = 0;
+      while ( (nr = stream->readSome(buf, sizeof(buf))) > 0)
       {
-        break;
+        int nw = stream->sendAll(buf, nr);
+        if (nw < nr)
+        {
+          break;
+        }
       }
-    }
+      printf("thread for no. %d client ended.\n", count);
+    }, std::move(tcpStream));
+    thr.detach();
   }
 }
