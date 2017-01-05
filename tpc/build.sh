5629ec0198494fb46d48c996742193e0c13e3e2d
Shuo Chen <chenshuo@chenshuo.com>
2017-01-05 15:36:24 +0800

    update tpc/build.sh

--- tpc/build.sh
+++ tpc/build.sh
@@ -6,26 +6,27 @@ SRCS="Acceptor.cc InetAddress.cc TcpStream.cc Socket.cc"
 
 set -x
 CC=${CC:-g++}
+CXXFLAGS="-std=c++11 -Wall -Wextra -g -O2"
 
-$CC -std=c++11 -Wall -Wextra -g -O2 $SRCS chargen.cc ../datetime/Timestamp.cc -o chargen -lpthread
+$CC $CXXFLAGS $SRCS chargen.cc ../datetime/Timestamp.cc -o chargen -lpthread
 
-$CC -std=c++11 -Wall -Wextra -g -O2 $SRCS discard.cc ../datetime/Timestamp.cc -o discard -lpthread
+$CC $CXXFLAGS $SRCS discard.cc ../datetime/Timestamp.cc -o discard -lpthread
 
-$CC -std=c++11 -Wall -Wextra -g -O2 $SRCS echo.cc -o echo -lpthread
+$CC $CXXFLAGS $SRCS echo.cc -o echo -lpthread
 
-$CC -std=c++11 -Wall -Wextra -g -O2 $SRCS echo_client.cc -o echo_client
+$CC $CXXFLAGS $SRCS echo_client.cc -o echo_client
 
-$CC -std=c++11 -Wall -Wextra -g -O2 $SRCS netcat.cc -o netcat -lpthread
+$CC $CXXFLAGS $SRCS netcat.cc -o netcat -lpthread
 
-$CC -std=c++11 -Wall -Wextra -g -O2 $SRCS nodelay.cc -o nodelay
+$CC $CXXFLAGS $SRCS nodelay.cc -o nodelay
 
-$CC -std=c++11 -Wall -Wextra -g -O2 $SRCS nodelay_server.cc -o nodelay_server
+$CC $CXXFLAGS $SRCS nodelay_server.cc -o nodelay_server
 
-$CC -std=c++11 -Wall -Wextra -g -O2 $SRCS roundtrip_udp.cc -o roundtrip_udp -lpthread
+$CC $CXXFLAGS $SRCS roundtrip_udp.cc -o roundtrip_udp -lpthread
 
-$CC -std=c++11 -Wall -Wextra -g -O2 $SRCS sender.cc -o sender -lpthread
+$CC $CXXFLAGS $SRCS sender.cc -o sender -lpthread
 
-$CC -std=c++11 -Wall -Wextra -g -O2 $SRCS sudoku_stress.cc ../datetime/Timestamp.cc -o sudoku_stress -lpthread
+$CC $CXXFLAGS $SRCS sudoku_stress.cc ../datetime/Timestamp.cc -o sudoku_stress -lpthread
 
-$CC -std=c++11 -Wall -Wextra -g -O2 $SRCS ttcp.cc -o ttcp -lboost_program_options
+$CC $CXXFLAGS $SRCS ttcp.cc -o ttcp -lboost_program_options
 
