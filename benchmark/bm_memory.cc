ad178f041fed5636d0dfcf30c39c498b9beef18f
Shuo Chen <chenshuo@chenshuo.com>
2019-03-09 23:23:38 -0800

    add BM_memcpy.

--- benchmark/bm_memory.cc
+++ benchmark/bm_memory.cc
@@ -25,9 +25,31 @@ static void BM_memset(benchmark::State& state)
   for (auto _ : state)
   {
     ::memset(buf, 0, len);
+    benchmark::DoNotOptimize(buf);
     benchmark::ClobberMemory();
   }
   state.SetBytesProcessed(len * state.iterations());
+  state.SetItemsProcessed(state.iterations());
   ::free(buf);
 }
 BENCHMARK(BM_memset)->Range(8, 1 << 30);
+
+// memory write bandwidth
+static void BM_memcpy(benchmark::State& state)
+{
+  int64_t len = state.range(0);
+  void* src = ::malloc(len);
+  ::memset(src, 0, len);
+  void* dst = ::malloc(len);
+  for (auto _ : state)
+  {
+    ::memcpy(dst, src, len);
+    benchmark::DoNotOptimize(dst);
+    benchmark::ClobberMemory();
+  }
+  state.SetBytesProcessed(len * state.iterations());
+  state.SetItemsProcessed(state.iterations());
+  ::free(dst);
+  ::free(src);
+}
+BENCHMARK(BM_memcpy)->Range(8, 1 << 30);
