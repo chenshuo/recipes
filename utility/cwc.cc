ccf7ef8b0f052215385313dc510a81e566623a2d
Shuo Chen <chenshuo@chenshuo.com>
2020-10-15 12:27:19 -0700

    cwc supports reading stdin.

--- utility/cwc.cc
+++ utility/cwc.cc
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <string.h>
 #include <unistd.h>
 
 using namespace std;
@@ -14,9 +15,23 @@ using namespace std;
 string readFile(const char* file)
 {
   string result;
+  // 128 KiB is from:
+  // http://git.savannah.gnu.org/cgit/coreutils.git/tree/src/ioblksize.h
+  // > As of May 2014, 128KiB is determined to be the minimium
+  // > blksize to best minimize system call overhead.
+  char buf[128*1024];
+  if (strcmp(file, "-") == 0) {
+    size_t nr = 0;
+    while ( (nr = fread(buf, 1, sizeof buf, stdin)) > 0) {
+      // printf("%zd\n", nr);
+      result.append(buf, nr);
+      if (feof(stdin))
+        break;
+    }
+    return result;
+  }
   int fd = open(file, O_RDONLY);
   if (fd >= 0) {
-    char buf[256*1024];
     ssize_t nr = 0;
     while ( (nr = read(fd, buf, sizeof buf)) > 0) {
       result.append(buf, nr);
@@ -36,12 +51,15 @@ enum Encoding
 
 const char* getEncodingName(Encoding enc)
 {
-  if (enc == kUTF8) {
-    return "UTF-8";
-  } else if (enc == kUnicode) {
-    return "Unicode";
-  } else {
-    return "Unknown";
+  switch (enc) {
+    case kUnicode:
+      return "Unicode";
+    case kUTF8:
+      return "UTF-8";
+    case kGBK:
+      return "GBK";
+    default:
+      return "Unknown";
   }
 }
 
@@ -129,23 +147,41 @@ int countChineseCharsUtf8(const string& content)
   return cnt;
 }
 
+int countChinese(const string& content, Encoding* enc)
+{
+  *enc = detectEncoding(content);
+  int chinese = 0;
+  if (*enc == kGBK) {
+    chinese = countChineseCharsGbk(content);
+  } else if (*enc == kUnicode) {
+    chinese = countChineseCharsUcs2(content);
+  } else {
+    chinese = countChineseCharsUtf8(content);
+  }
+
+  return chinese;
+}
+
 int main(int argc, char* argv[])
 {
   int totalLines = 0;
   int totalChinese = 0;
   size_t totalBytes = 0;
+  if (argc <= 1) {
+    const char* file = "-";
+    string content = readFile(file);
+    Encoding enc = kUnknown;
+    int chinese = countChinese(content, &enc);
+    int lines = std::count(content.begin(), content.end(), '\n');
+    printf("%6d %6d %7zd %s (%s)\n",
+        lines, chinese, content.size(), file, getEncodingName(enc));
+    return 0;
+  }
   for (int i = 1; i < argc; ++i) {
     const char* file = argv[i];
     string content = readFile(file);
-    Encoding enc = detectEncoding(content);
-    int chinese = 0;
-    if (enc == kGBK) {
-      chinese = countChineseCharsGbk(content);
-    } else if (enc == kUnicode) {
-      chinese = countChineseCharsUcs2(content);
-    } else {
-      chinese = countChineseCharsUtf8(content);
-    }
+    Encoding enc = kUnknown;
+    int chinese = countChinese(content, &enc);
     int lines = std::count(content.begin(), content.end(), '\n');
     printf("%6d %6d %7zd %s (%s)\n",
         lines, chinese, content.size(), file, getEncodingName(enc));
