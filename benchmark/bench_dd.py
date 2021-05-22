c65da8c5c76eb151550bd89aef71450a1085af97
Shuo Chen <chenshuo@chenshuo.com>
2021-05-22 03:11:06 +0000

    bench_dd.py parses dd(1) output on FreeBSD.

--- benchmark/bench_dd.py
+++ benchmark/bench_dd.py
@@ -7,9 +7,16 @@ count = 1024 * 1024
 while bs <= 1024 * 1024 * 8:
     args = ['dd', 'if=/dev/zero', 'of=/dev/null', 'bs=%d' % bs, 'count=%d' % count]
     result = subprocess.run(args, capture_output=True)
-    m = re.search('copied, (.*?) s, ', str(result.stderr))
-    seconds = float(m.group(1))
-    print('bs=%7d count=%7d %6.3fs %7.3fus/record %9.3fMB/s' %
+    seconds = 0
+    message = str(result.stderr)
+    if m := re.search('copied, (.*?) s, ', message):
+        seconds = float(m.group(1))
+    elif m := re.search('bytes transferred in (.*?) secs', message):
+        seconds = float(m.group(1))
+    else:
+        print('Unable to parse dd output:\n%s' % message)
+        break
+    print('bs=%7d count=%7d %6.3fs %8.3fus/record %9.3fMB/s' %
             (bs, count, seconds, seconds * 1e6 / count, bs * count / 1e6 / seconds))
 
     bs *= 2
